4ca1262c70b5f83d65d35a6fe993f9534151af83
stas <point.blank.point@bk.ru>
2023-05-21 19:08:39 +0300

    u3 ticket

--- lesson1/u3.py
+++ lesson1/u3.py
@@ -2,8 +2,8 @@
 # 385916
 
 ticket = input('Введите номер билета: ')
-firsThird = int(t[0]) + int(t[1]) + int(t[2])
-lastThird = int(t[3]) + int(t[4]) + int(t[5])
+firsThird = int(ticket[0]) + int(ticket[1]) + int(ticket[2])
+lastThird = int(ticket[3]) + int(ticket[4]) + int(ticket[5])
 if firsThird == lastThird:
     print('Yes')
 else:
